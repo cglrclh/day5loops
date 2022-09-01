3dc9a01fde8fcef44b2558b13e4abedc07953569
cglrclh <caglarculha35@gmail.com>
2022-09-02 01:47:57 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int sayi = int.Parse(Console.ReadLine());
+        for (int i = 1; i <= 10; i++)
+        {   
+            Console.WriteLine(sayi + " x " + i + " = " +  sayi*i  );
+        }
+    }
+}
